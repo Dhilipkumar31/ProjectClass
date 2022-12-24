327b1e973da33f8ad98e09bb0364ba7bdd0a3c27
Dhilipkumar31 <rajdhilip2287434@gmail.com>
2022-12-24 19:54:28 +0530

    Kumar's code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -1,10 +1,12 @@
 package org.test;
 
 public class Sample {
-	
+
 	public static void main(String[] args) {
-		System.out.println("Test 2");
-	
+		System.out.println("Test 1");
+		System.out.println("Test 4");
+		System.out.println("Test 5");
+
 	}
 
 }
